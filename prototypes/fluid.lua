a27732c47cadf718126bd12ee6e183346d678881
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-02 22:05:45 -0700

    forgot a comma

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -7,7 +7,7 @@ local emcFluid = {
     flow_color = {r=0.7, g=0.7, b=0.7},
     icon = "__base__/graphics/icons/fluid/water.png",
     icon_size = 32,
-    subgroup = "emc-tokens"
+    subgroup = "emc-tokens",
     order = "a[fluid]-a[water]",
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59
