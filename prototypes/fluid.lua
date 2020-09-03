1cf2ae713cf541ee604f416e819643718f67eb94
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-02 22:26:38 -0700

    resized emc fluid icon

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -6,7 +6,7 @@ local emcFluid = {
     base_color = {r=0, g=0.34, b=0.6},
     flow_color = {r=0.7, g=0.7, b=0.7},
     icon = "__base__/graphics/icons/fluid/water.png",
-    icon_size = 32,
+    icon_size = 64,
     subgroup = "emc-tokens",
     order = "a[fluid]-a[water]",
     pressure_to_speed_ratio = 0.4,
