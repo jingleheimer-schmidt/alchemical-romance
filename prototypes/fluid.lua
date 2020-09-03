57d9a8c9576a90ec92fff0a2a09187a648da09cd
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-02 21:54:05 -0700

    added EMC fluid

---
+++ prototypes/fluid.lua
@@ -0,0 +1,18 @@
+local emcFluid = {
+    type = "fluid",
+    name = "emc",
+    default_temperature = 15,
+    max_temperature = 100,
+    base_color = {r=0, g=0.34, b=0.6},
+    flow_color = {r=0.7, g=0.7, b=0.7},
+    icon = "__base__/graphics/icons/fluid/water.png",
+    icon_size = 32,
+    subgroup = "emc-tokens"
+    order = "a[fluid]-a[water]",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59
+  }
+
+  data:extend({
+    emcFluid
+  })
