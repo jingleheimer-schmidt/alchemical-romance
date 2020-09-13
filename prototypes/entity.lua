3e847100eccc4ccc5bcd4fea23de29a240c83978
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-12 18:13:00 -0700

    removed working_visualizations from combiner since factorio doesn't support that property tag

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -250,38 +250,28 @@ local chemicalCombiner = {
             height = 219,
             frame_count = 1
           }
-        }
-      }
-    },
-    working_visualisations =
-    {
-      {
-        animation =
+        },
+        {
+          filename = "__alchemical-romance__/graphics/machines/chemical-combiner/lr-mc-chemical-combiner-working-animation-loop.png",
+          priority = "high",
+          width = 108,
+          height = 119,
+          -- FIX THIS: --
+          frame_count = 14,
+          line_length = 7,
+          shift = util.by_pixel(0, -0.5),
+          hr_version =
           {
-            layers =
-            {
-              filename = "__alchemical-romance__/graphics/machines/chemical-combiner/lr-mc-chemical-combiner-working-animation-loop.png",
-              priority = "high",
-              width = 108,
-              height = 119,
-              frame_count = 14,
-              line_length = 7,
-              shift = util.by_pixel(0, -0.5),
-              hr_version =
-              {
-                filename = "__alchemical-romance__/graphics/machines/chemical-combiner/hr-mc-chemical-combiner-working-animation-loop.png",
-                priority = "high",
-                width = 239,
-                height = 219,
-                frame_count = 14,
-                line_length = 7,
-                shift = util.by_pixel(0, -0.75),
-                scale = 0.5
-              }
-            }
+            filename = "__alchemical-romance__/graphics/machines/chemical-combiner/hr-mc-chemical-combiner-working-animation-loop.png",
+            priority = "high",
+            width = 239,
+            height = 219,
+            frame_count = 14,
+            line_length = 7,
+            shift = util.by_pixel(0, -0.75),
+            scale = 0.5
           }
-        }
-      }
+        },
         -- {
         --   filename = "__base__/graphics/entity/assembling-machine-3/assembling-machine-3-shadow.png",
         --   priority = "high",
@@ -304,7 +294,9 @@ local chemicalCombiner = {
         --     scale = 0.5
         --   }
         -- }
+      }
     }
+}
 
 --== Energy Condenser ==--
 
